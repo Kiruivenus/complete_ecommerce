5fb43cd2fe04f26f9d48e979b736793cb4748e47
Kiruivenus <139698673+Kiruivenus@users.noreply.github.com>
2024-11-26 12:02:59 +0300

    payment fix

--- server/helpers/paypal.js
+++ server/helpers/paypal.js
@@ -2,8 +2,8 @@ const paypal = require("paypal-rest-sdk");
 
 paypal.configure({
   mode: "sandbox",
-  client_id:"Aa6jUCqUo0TlEUEZnK7ucu9u_8H0Jl6NRvlzNey7uYmgADW03WqhtDBG5IRDn-H1P5_ko7tXAenb1ut8",
-  client_secret:"EEdLB8GwR9ZRbmGJ8zpkEKHQ93w5zJIuwSpfqUuHZ3zqWLW01a_o-XrkjgjltuMlV1DSoSqIkgFhtZVK",
+  client_id: process.env.PAYPAL_CLIENT_ID,
+  client_secret: process.env.PAYPAL_CLIENT_SECRET,
 });
 
 module.exports = paypal;
